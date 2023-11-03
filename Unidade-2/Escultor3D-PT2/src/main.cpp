127fc22b9164e891cd309fd69c4c691d69c9ba00
Hugo Henrique <143297587+hugo-henriq21@users.noreply.github.com>
2023-11-03 01:58:48 -0300

    Add files via upload

---
+++ Unidade-2/Escultor3D-PT2/src/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"sculptor.hpp"
+#include"FiguraGeometrica.hpp"
+#include"ReadFromFile.hpp"
+
+using namespace std;
+
+int main(void){
+
+    ReadFromFile("sculptor.txt","output.off");
+
+    return 0;
+
+};
