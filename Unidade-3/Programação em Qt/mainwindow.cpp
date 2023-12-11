06c4d7aec99184064ba040b09bc6273d96dc30de
Hugo Henrique <143297587+hugo-henriq21@users.noreply.github.com>
2023-12-10 23:04:39 -0300

    Add files via upload

---
+++ Unidade-3/Programação em Qt/mainwindow.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <cstdlib>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui -> Quit,
+            SIGNAL (clicked(bool)),
+            this,
+            SLOT(fechar()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::fechar()
+{
+    exit(0);
+}
+
+void MainWindow::copiaTexto()
+{
+    ui->textBrowser-> setText(ui->textEdit->toPlainText());
+}
